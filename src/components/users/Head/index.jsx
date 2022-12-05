243e06fe4739052ea8e5269ec00b1fcd0844e64a
Nicholasjr <nicholasduadei14@gmail.com>
2022-12-05 08:10:36 +0100

    business Details

---
+++ src/components/users/Head/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const index = () => {
+  return (
+    <div className='p-4 px-3 bg-white'>
+        <h1 className='text-2xl text-[#69707D] font-medium'>
+            Dashboard
+        </h1>
+    </div>
+  )
+}
+
+export default index
