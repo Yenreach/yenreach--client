4bb0668d01a44550dd615e466948f65820c02a4b
Nicholasjr <nicholasduadei14@gmail.com>
2023-02-23 00:45:41 +0100

    api services

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -6,9 +6,35 @@ const serviceSuffix = ".php"
 const token = JSON.parse(localStorage.getItem("user"))?.verify_string
 
 
+/* Get user */
+export const apiGetUser = () => {
+    return ApiAdapter.fetchData({
+        url: `fetch_user_by_string_api${serviceSuffix}?string=${token}`,
+        method: "get"    
+    })
+}
+
+/* Update user */
+export const apiUpdateUser = (data) => {
+    return ApiAdapter.fetchData({
+        url: `edit_user_profile_api${serviceSuffix}`,
+        method: "post",
+        data
+    })
+}
+
+/* Activity Log */
+export const apiAddActivityLog = (data) => {
+    return ApiAdapter.fetchData({
+        url: `add_activity_log_api${serviceSuffix}`,
+        method: "post",
+        data
+    })
+}
+
 /* Get user businesses */
 export const apiGetAllBusinesses = () => {
-    console.log("token", token)
+    // console.log("token", token)
     return ApiAdapter.fetchData({
         url: `fetch_business_by_user_string_api${serviceSuffix}?string=${token}`,
         method: "get"    
