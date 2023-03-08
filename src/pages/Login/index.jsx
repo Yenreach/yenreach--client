9fab1d74940cf9326c67088b88e66f93c71c81b6
Nicholasjr <nicholasduadei14@gmail.com>
2023-03-08 20:14:05 +0100

    login style update

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
-import { Route, Navigate, useLocation } from 'react-router-dom'
+import { Navigate, useLocation } from 'react-router-dom'
 import { useAuthContext } from '/src/hooks/useAuthContext'
-import PasswordSvg from '../../assets/password.svg';
+import { BsEye, BsEyeSlash } from 'react-icons/bs';
 import Button from '../../components/ui/Button';
 import useLogin from '../../hooks/useLogin';
 
 const index = () => {
   const [email, setEmail] = React.useState('');
   const [password, setPassword] = React.useState('');
+  const [showPassword, setShowPassword] = React.useState(false);
 
   const { user } = useAuthContext()
   const location = useLocation()
   
   const { login, error, isLoading, setError } = useLogin();
+
 
   if (user) {
     return <Navigate to={{ pathname: '/users', state: { from: location } }} />
@@ -33,8 +35,8 @@ const index = () => {
           <div className="flex flex-col gap-1">
             <label className='text-sm font-semibold' htmlFor="name">Password</label>
             <div className="relative w-full">
-              <input onChange={(event) => setPassword(event.target.value)} className='border-2 p-2 w-full border-[#BABFC5] bg-[#f5f5f791] rounded-md' type="text" name='name' id='name' />
-              <img className='absolute top-0 bottom-0 m-auto right-3 scale-75' src={PasswordSvg} alt="" />
+              <input onChange={(event) => setPassword(event.target.value)} className='border-2 p-2 w-full border-[#BABFC5] bg-[#f5f5f791] rounded-md' type={showPassword ? "text" : "password"} name='password' id='password' />
+              {!showPassword? <BsEye onClick={() => setShowPassword(true)} className='absolute top-0 bottom-0 m-auto right-3 z-20 scale-110 cursor-pointer' /> : <BsEyeSlash onClick={() => setShowPassword(false)} className='absolute top-0 bottom-0 m-auto right-3 z-20 scale-110 cursor-pointer' />}
             </div>
             <p className="w-full text-xs text-[#FF6B93] text-right">Forgot Password?</p>
           </div>
