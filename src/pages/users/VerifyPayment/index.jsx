4fcabb23350390702522dce2913100b06c7e484d
Nicholasjr <nicholasduadei14@gmail.com>
2023-04-03 20:59:43 +0100

    paymnt verification

--- src/pages/users/VerifyPayment/index.jsx
+++ src/pages/users/VerifyPayment/index.jsx
@@ -1,52 +1,60 @@
-import React, { useEffect, useParams } from 'react'
-import { Navigate } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { useParams, Navigate, useSearchParams, useNavigate } from 'react-router-dom'
 import { apiVerifyPayment } from '/src/services/SubscriptionService'
 import usePost from '/src/hooks/usePost'
 import Loading from '/src/components/Loader'
 
 
 const VerifyPayment = () => {
-    const { id, tx_ref, transaction_id } = useParams()
+    const [searchParams, setSearchParams] = useSearchParams()
+    const navigate = useNavigate()
+    const tranx_id = searchParams.get("transaction_id")
+    const txref = searchParams.get("tx_ref")
 
 
     const { data, error, isLoading, isSuccess, mutate, status,} = usePost({ api: apiVerifyPayment })
  
     useEffect(() => {
         const verify = async () => {
-            await mutate({ id, tx_ref, transaction_id })
+            console.log("tx", txref, tranx_id)
+            await mutate({ txref, tranx_id })
         }
 
-        if (id && tx_ref && transaction_id) {
+        if (txref && tranx_id) {
             verify()
         }
-    }, [id, tx_ref, transaction_id])
+    }, [txref, tranx_id])
 
-    if (isLoading) {
-        return <div>Loading...</div>
-    }
+    // if (isLoading) {
+    //     return <div>Loading...</div>
+    // }
+
+    console.log("verify transaction data", data)
+
 
     useEffect(() => {
         if (isSuccess || data) {
+            console.log("redirecting function", data)
             const redirectFunc = () => {
                 if(data?.reason == "business_subscription"){
-                    redirect_to(`https://yenreach.com/users/business_subscription?string=${data?.verify_string}&method=online_payment`);
+                    return navigate(`/users/subscription/${data?.verify_string}?string=${data?.verify_string}&method=online_payment`);
                 } else if(data?.reason == "business_subscription_renewal"){
-                    redirect_to(`https://yenreach.com/users/business_subscription_renewal?string=${data?.verify_string}&method=online_payment`);
+                    return navigate(`/users/subscription/${data?.verify_string}_renewal?string=${data?.verify_string}&method=online_payment`);
                 } else if(data?.reason == "billboard_payment"){
-                    redirect_to(`https://yenreach.com/users/billboard_apply?string=${data?.verify_string}&method=online_payment`);
+                    return navigate(`/users/billboard_apply?string=${data?.verify_string}&method=online_payment`);
                 } else {
                     $session?.message("No Payment Reason was provided");
-                    redirect_to("failure.php");
+                    return navigate("failure.php");
                 }
-                return <Navigate to="/dashboard" />
+                return navigate(`/users`)
             }
             setTimeout(redirectFunc, 3000)
         }
     }, [isSuccess, data])
 
     return (
-        <div className='flex justify-center items-center gap-6'>
-            {Loading && <Loading /> }
+        <div className='flex justify-center items-center gap-6 h-screen'>
+            {isLoading && <Loading /> }
             {isSuccess && (
                 <div className='flex flex-col justify-center items-center gap-2'>
                     <h1>Payment Successful</h1>
