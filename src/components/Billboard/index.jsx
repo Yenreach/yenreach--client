ff40dbc2a15baf259c4f346358edb72da5e52aaf
Nicholasjr <nicholasduadei14@gmail.com>
2023-04-04 21:58:13 +0100

    .

--- src/components/Billboard/index.jsx
+++ src/components/Billboard/index.jsx
@@ -1,8 +1,8 @@
 import React, { useRef, useEffect, useState } from 'react'
 import Computer from '/src/assets/computer.svg'
-// import DECImg from '/src/assets/DEC_Image.jpg'
-// import LogoImg from '/src/assets/LOGO_4491659091525.jpg'
-// import AdImg from '/src/assets/ads-image.jpg'
+import DECImg from '/src/assets/DEC_Image.jpg'
+import LogoImg from '/src/assets/LOGO_4491659091525.jpg'
+import AdsImg from '/src/assets/adsimage.jpg'
 import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
 import 'pure-react-carousel/dist/react-carousel.es.css';
 
@@ -12,37 +12,45 @@ const billboards = [
         id: 1,
         title: 'Dordorian Estate Limited',
         description: 'Own your dream Property in Fast developing locations across Yenagoa, Bayelsa State.',
-        image: './assets/img/DEC_Image.jpg',
+        image: DECImg,
         link: 'https://dordorianestate.com'
     },
     {
         id: 2,
         title: 'Globus Bank',
         description: 'A commercial bank with National Authorization. Registered as a limited liability company on March 6, 2019, licensed by the Central Bank of Nigeria on July 10, 2019 and commenced operations on November 6, 2019.',
-        image: './images/LOGO_4491659091525.jpg',
-        link: 'business?3b103aaa17b1e28da751caa93e1c67aa11515838'
+        image: LogoImg,
+        link: '/business/3b103aaa17b1e28da751caa93e1c67aa11515838'
     },
     {
         id: 3,
         title: 'Mutual Benefits Life Assurance Limited',
         description: 'One of Nigeria\'s leading life Insurance provider with products ranging from, *Children Education Plan *Individual Savings Plan *Group life *General Third Party Liability *Professional Indemnity *Public Liability *Comprehensive Car Insurance *Mutual Term Assurance *Goods -in-transit E.T.C',
-        image: './assets/img/clients/business/Screenshot_20211103-132259.png',
-        link: 'business?bfd9cc92264da47e591e505e860f143ef5d69eba'
+        image: Computer,
+        link: '/business/bfd9cc92264da47e591e505e860f143ef5d69eba'
     },
     // {
     //     id: 4,
     //     title: 'Data Science Nigeria',
     //     description: 'Data Science Nigeria is a community of data scientists, data analysts, data engineers, data architects, data visualizers, and data enthusiasts. We are a community of data scientists, data analysts, data engineers, data architects, data visualizers, and data enthusiasts.',
     //     image: './assets/img/clients/business/Screenshot_20211103-132259.png',
-    //     link: 'business?bfd9cc92264da47e591e505e860f143ef5d69eba'
+    //     link: '/business/bfd9cc92264da47e591e505e860f143ef5d69eba'
     // },
     // {
     //     id: 5,
     //     title: 'Data Science Nigeria',
     //     description: 'Data Science Nigeria is a community of data scientists, data analysts, data engineers, data architects, data visualizers, and data enthusiasts. We are a community of data scientists, data analysts, data engineers, data architects, data visualizers, and data enthusiasts.',
     //     image: './assets/img/clients/business/Screenshot_20211103-132259.png',
-    //     link: 'business?bfd9cc92264da47e591e505e860f143ef5d69eba'
+    //     link: '/business/bfd9cc92264da47e591e505e860f143ef5d69eba'
     // },
+    {
+        id: 6,
+        title: 'Advertise your business here',
+        description: 'Advertise your business here',
+        image: AdsImg,
+        link: '/users/billboard'
+    }
+
 ]
 
 const Billboard = () => {
@@ -92,7 +100,7 @@ const Billboard = () => {
         naturalSlideHeight={125}
         totalSlides={billboards.length}
         infinite={true}
-        interval={3000}
+        interval={6000}
         isPlaying={true}
         className='transition-all duration-1000 ease h-[65vh] relative'
       >
@@ -102,7 +110,7 @@ const Billboard = () => {
                 <div key={billboard?.id} className=" h-[65vh]">
                     <div className='h-full w-full flex flex-col md:flex-row gap-8 justify-center items-center'>
                         <div className="flex-1 h-full w-full">
-                            <img src={Computer} alt="advert-banner" className='w-100 h-full object-cover carousel-img' />
+                            <img src={billboard?.image} alt="advert-banner" className='w-100 h-full object-cover carousel-img' />
                         </div>
                         <div className="flex flex-col gap-4 justify-center items-center flex-1 h-full w-full text-center">
                             <h2 className="text-xl lg:text-3xl text-capitalize text-bold text-black">{billboard?.title}</h2>
