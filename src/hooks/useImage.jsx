9cbd9a1303b09b78f5f66d66eeb64e793079581f
DevNickJr <nicholasduadei14@gmail.com>
2023-04-27 17:11:48 +0100

    404 page update

--- src/hooks/useImage.jsx
+++ src/hooks/useImage.jsx
@@ -27,7 +27,7 @@ const useImage = () => {
             // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
             const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
             setProgress(progress)
-            console.log('Upload is ' + progress + '% done');
+            // console.log('Upload is ' + progress + '% done');
     
             switch (snapshot.state) {
                 case 'paused':
--- src/pages/404/index.jsx
+++ src/pages/404/index.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import ErrorImg from '/src/assets/error-page.png'
+import { Link } from 'react-router-dom'
 
 const index = () => {
   return (
-    <div>
-        Oga You too Sabi.. Go back
-        <a href="/">Home</a>
+    <div className='flex justify-center items-center flex-col gap-12 h-screen p-12 text-center -mt'>
+        <img src={ErrorImg} alt="" className='w-full max-w-xl' />
+        <p>The page you're requesting for does not exist</p>
+        <Link to="/" className='underline text-green'> Go back Home</Link>
     </div>
   )
 }
