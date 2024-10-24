0da329142a5d01967df9ef9bb6295637699d6a29
DevNickjr <nicholasduadei14@gmail.com>
2024-10-24 14:17:45 +0100

    .

--- src/pages/Business/index.jsx
+++ src/pages/Business/index.jsx
@@ -174,7 +174,7 @@ const index = () => {
             // imageUrl={business.image || '/default-image.png'}
           />
           <div className={`top mb-10 py-16 sm:py-12 px-4 md:px-10 lg:px-20 relative ${business?.cover_img ? "" : 'bg-[url("assets/businesses/business-hero.svg")]'} bg-cover bg-center text-white flex items-center gap-5`}>
-             {business?.cover_img && <img src={business?.profile_img.replace("mediatoken", "media&token")} alt="" className='absolute top-0 left-0 w-full h-full -z-10' />}
+             {business?.cover_img && <Image src={business?.profile_img.replace("mediatoken", "media&token")} alt="" className='absolute top-0 left-0 w-full h-full -z-1' />}
             <Image
                 url={business?.profile_img}
                 name={business?.name}
