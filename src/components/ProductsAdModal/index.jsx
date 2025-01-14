c47cf1ffa8d96db11df1987e7153c25be186c732
DevNickjr <nicholasduadei14@gmail.com>
2025-01-14 19:09:09 +0100

    fix scroll on ads modal close

--- src/components/ProductsAdModal/index.jsx
+++ src/components/ProductsAdModal/index.jsx
@@ -19,15 +19,15 @@ const ProductAdsModal = ({ setModalOpen, modalOpen }) => {
     }
 
     const closeModal = () => {
-        setModalOpen(false)
         document.body.style.overflow = 'unset'
+        setModalOpen(false)
     }
 
 
   return (
     <div onClick={handleOutsideClick} className='backdrop-blur-[1.5px] bg-black/60 fixed top-0 left-0 w-full max-h-screen min-h-screen z-50 grid place-items-center overflow-hidden p-8 max-w-screen'>
         <div className="relative w-full p-8 bg-white lg:max-w-6xl">
-            <MdClose className='absolute text-xl cursor-pointer top-2 right-2 md:top-8 md:right-8' onClick={() => setModalOpen(false)} />
+            <MdClose className='absolute text-xl cursor-pointer top-2 right-2 md:top-8 md:right-8' onClick={() => closeModal()} />
             <h2 className='mb-6 text-xl font-semibold text-center text-orange'>Products you may like</h2>
             <Slide setModalOpen={setModalOpen} />
         </div>
