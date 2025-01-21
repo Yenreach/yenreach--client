c066b2a91e044c22f867f45a9d9d291efb946d6a
DevNickjr <nicholasduadei14@gmail.com>
2025-01-21 09:18:43 +0100

    .

--- src/components/ProductsAdModal/index.jsx
+++ src/components/ProductsAdModal/index.jsx
@@ -34,7 +34,7 @@ const ProductAdsModal = ({ setModalOpen, modalOpen }) => {
   return (
     <div onClick={handleOutsideClick} className='backdrop-blur-[1.5px] bg-black/60 fixed top-0 left-0 w-full max-h-screen min-h-screen z-50 grid place-items-center overflow-hidden p-8 max-w-screen'>
         <div className="relative w-full p-8 bg-white lg:max-w-6xl">
-            <MdClose className='absolute text-xl cursor-pointer top-2 right-2 md:top-8 md:right-8' onClick={() => closeModal()} />
+            <MdClose className='absolute z-50 text-xl cursor-pointer top-2 right-2 md:top-8 md:right-8' onClick={() => closeModal()} />
               {
                 !isOPen &&
             <h2 className='mb-6 text-xl font-semibold text-center text-orange'>Products you may like</h2>
